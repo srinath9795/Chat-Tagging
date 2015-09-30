8b3382a2b99418996d4421373876fa2e5aa9d675
menirus <surya.cse.iitkgp@gmail.com>
2015-10-01 01:51:41 +0530

    SVM Classification added

--- classification.py
+++ classification.py
@@ -2,6 +2,7 @@ import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning) 
 from nltk.stem.wordnet import WordNetLemmatizer
 import numpy as np
+from sklearn import svm
 import lda
 #import lda.datasets
 import re
@@ -22,27 +23,38 @@ pp = pprint.PrettyPrinter()
 import json
 import pprint
 pp = pprint.PrettyPrinter()
-jf = open('item.json')
-items = json.load(jf)
+
+
+"""Below code reads the file, combines 5 messages into into sentence and returns the list of such sentences in list named 'docs' """
+
+file_names = ['sports.txt','technology.txt']
 cnt = 0
 docs = []
 tmp = ""
-prev = 0
-for i in items:
-    tmp += i["message"]+" "
-    cnt+=1
-    if cnt>=5 and i["nick"]!=prev:
-        docs += [tmp]
-        tmp = ""
-        cnt = 0
-    prev = i["nick"]
 
+for fn in file_names:
+    jf = open('reddit_data/'+fn,'r')
+    for i in jf:
+        tmp += i+" "
+        cnt+=1
+        if cnt==5:
+            docs += [(tmp,fn)]
+            tmp = ""
+            cnt = 0
+""" File reading done """
+print len(docs)
+docs = np.array(docs)
+np.random.shuffle(docs)
+docs = docs[:6000,:]
+
+""" Remove stop words and build vocabulary """
 removelist = "=& "  # for non-alpha numeric chars that are allowed
 msgCount = 0
 doc_freq_dic = []
 vocab = defaultdict(int)
 stop = stopwords.words('english')
 for msgSet in docs:
+    msgSet = msgSet[0]
     msgSet= re.sub(r'[^\w'+removelist+']', '',msgSet).lower()
     lmtzr = WordNetLemmatizer()
     words = map(lmtzr.lemmatize,msgSet.split())
@@ -55,17 +67,10 @@ for msgSet in docs:
     # print msgSet
     # break
 vocab=dict(vocab)
+""" vocab building done """
+
 vocabList = [key for key in vocab if vocab[key]>1]
-# for key in vocab:
-#   if vocab[key]>1:
-#       vocabList.append(key)
-# sorted_x = sorted(vocab.items(), key=operator.itemgetter(1))
-# print sorted_x
 print len(vocabList)
-
-
-
-
 
 
 ldaInput = []
@@ -76,15 +81,30 @@ for i in range(len(docs)):
             vector.append(doc_freq_dic[i][vocabList[j]])
         else:
             vector.append(0)
+    vector.append(file_names.index(docs[i][1]))
     ldaInput.append(vector)
+
 ldaInput = np.array(ldaInput)
-model = lda.LDA(n_topics=20, n_iter=500, random_state=1)
-model.fit(ldaInput)
-topic_word = model.topic_word_
-for n in range(5):
-    sum_pr = sum(topic_word[n,:])
-    print("topic: {} sum: {}".format(n, sum_pr))
-n = 10
-for i, topic_dist in enumerate(topic_word):
-    topic_words = np.array(vocabList)[np.argsort(topic_dist)][:-(n+1):-1]
-    print('*Topic {}\n- {}'.format(i, ' '.join(topic_words)))
+
+train_x = ldaInput[:3000,:-1]
+train_y = ldaInput[:3000,-1]
+
+test_x = ldaInput[3000:6000,:-1]
+test_y = ldaInput[3000:6000,-1]
+
+print train_x.shape,train_y.shape,test_x.shape,test_y.shape
+
+clf = svm.SVC()
+clf.fit(train_x,train_y)
+
+ans_y = clf.predict(test_x)
+
+miss = 0
+for i in range(ans_y.shape[0]):
+    if ans_y[i] != test_y[i]:
+        miss += 1
+        print docs[3000+i][0],file_names[test_y[i]],file_names[ans_y[i]]
+print "Total misses : " + str(miss)
+
+
+
