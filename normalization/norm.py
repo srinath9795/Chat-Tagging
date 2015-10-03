64a14de65a3b6b998877ae2b9ce4402dbf57bb1d
menirus <surya.cse.iitkgp@gmail.com>
2015-10-04 00:53:12 +0530

    Server addition and normalization with infinite input

--- normalization/norm.py
+++ normalization/norm.py
@@ -147,10 +147,10 @@ def normalize(sms):
 	translated = []
 	for token in tokens:
 		if token in translationMap:
-			print 'already present in map',token,translationMap[token]
+#			print 'already present in map',token,translationMap[token]
 			translated.append(translationMap[token])
 		elif token in englishVocab:
-			print 'already present in vocab'
+#			print 'already present in vocab'
 
 			translated.append(token)
 		else:
@@ -159,7 +159,7 @@ def normalize(sms):
 				r=probw(token,word,d)
 				wp.append((word,r))
 			wp=sorted(wp,key=lambda x: x[1])
-			print 'norm done',token,wp[-1][0]
+			#print 'norm done',token,wp[-1][0]
 
 			translated.append(wp[-1][0])
 	return ' '.join(translated)
@@ -184,100 +184,6 @@ def normalize(sms):
 # print "d['$to']",d['$to'],d['t']
 # print d['morro']
 
+while True:
+	print normalize(raw_input())
 
-
-@app.route('/norm',methods=['POST'])
-def giveans():
-	strings=request.data
-	jsonobj=json.loads(strings)
-	print jsonobj
-	print strings
-	return 'working'
-
-@app.route('/')
-def response():
-	return 'working'
-
-
-import re
-import operator
-from nltk.stem.wordnet import WordNetLemmatizer
-from nltk import word_tokenize
-
-f = open('../stopwords.txt','r')
-stops = [l.strip() for l in f]
-f.close()
-
-file_names = ['sports','technology','movies','music','politics','programming']
-sub_cats = {}
-sub_cats['sports'] = ['nba','soccer']
-
-top_words_dict = {}
-
-for fn in file_names:
-	f = open('../reddit_top_words/'+fn,'r')
-	tmp = [(l.split()[0],int(l.split()[1])) for l in f]
-	top_words_dict[fn] = dict(tmp)
-	f.close()
-
-for fn in sub_cats['sports']:
-	f = open('../reddit_top_words/'+fn,'r')
-	tmp = [(l.split()[0],int(l.split()[1])) for l in f]
-	top_words_dict[fn] = dict(tmp)
-	f.close()
-
-print len(top_words_dict)
-
-cnt = 0
-tmp = ""
-
-@app.route('/tag',methods=['POST'])
-def tag():
-	jsono=request.data
-	jdic=json.loads(jsono)
-	msg = " ".join(jdic['arrayy'])
-	removelist = "=& "
-	msg = re.sub(r'[^\w'+removelist+']', '',msg).lower()
-	lmtzr = WordNetLemmatizer()
-    	words = map(lmtzr.lemmatize,msg.split())
-	freq_dic = {}
-	freq_dic_ind = {}
-	for word in words:
-		if word not in stops:
-			for topic in file_names:
-				if word in top_words_dict[topic]:
-					if topic not in freq_dic:
-						freq_dic[topic] = 0
-						freq_dic_ind[topic] = []
-								
-					freq_dic[topic] += top_words_dict[topic][word]
-					freq_dic_ind[topic] += [(word,top_words_dict[topic][word])]
-			
-	print freq_dic
-	
-	if len(freq_dic)==0:
-		print "None"
-	else:
-		tag = max(freq_dic.iteritems(), key=operator.itemgetter(1))[0]
-		if tag == "sports":
-			freq_dic = {}
-			freq_dic_ind = {}
-			for word in words:
-				if word not in stops:
-					for topic in sub_cats['sports']:
-						if word in top_words_dict[topic]:
-							if topic not in freq_dic:
-								freq_dic[topic] = 0
-								freq_dic_ind[topic] = []
-							
-							freq_dic[topic] += top_words_dict[topic][word]
-							freq_dic_ind[topic] += [(word,top_words_dict[topic][word])]
-			sub_tag = max(freq_dic.iteritems(), key=operator.itemgetter(1))[0]
-			print freq_dic,freq_dic_ind
-			print tag, sub_tag
-	cnt = 0
-	tmp = ""
-	return tag
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=10000,debug=True)
